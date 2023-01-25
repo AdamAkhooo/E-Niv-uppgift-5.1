8c184ff559b37ed7c8458960f7d1056e2bd0ac50
Admin <Admin@TEBL123005>
2023-01-25 11:45:50 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[]args)
+    {
+        
+        string[] namn = new string[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"skriv in ett namn {i + 1}");
+                namn[i] = Console.ReadLine();
+            }
+        Console.WriteLine("Här är de namnen du har valt");
+        for(int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"\n{namn[i]}");
+        }
+            
+
+        
+        
+    }
+}
